6dc451ff6131204d044d28dcb1695d6e729e1a98
anacarolinesilva <anasilva.caroline@hotmail.com>
2021-10-25 18:35:37 -0300

    commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+* Hora do dia
+* Vamos ver se estamos de bom dia, boa tarde ou boa noite
---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuto}.`
+    if (hora >= 0 && hora <= 12) {
+        // BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#8f7c74'
+    } else if (hora > 12 && hora <= 18) {
+        // BOA TARDE
+        img.src = 'tarde.png'
+        document.body.style.background = '#4f3a41'
+    } else {
+        // BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background = '#182c33'
+    }
+}
